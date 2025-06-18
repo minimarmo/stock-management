9cf7b2bf925e969569148f14f4d885ca9fa3f6fe
marmo <kaew.renuka@gmail.com>
2025-06-18 16:46:21 +0700

    Setup PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { registerSW } from "virtual:pwa-register";
+registerSW();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/stock-management/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Stock Management",
+        short_name: "Stock App",
+        start_url: "/stock-management/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#7649DF", // สี app bar
+        icons: [
+          {
+            src: "/stock.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/stock.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
